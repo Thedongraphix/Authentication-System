a890eee790b71343e29a6f7cf2c64076c68e480f
Thedongraphix <chrisoketchcn@gamil.com>
2024-09-06 13:55:35 +0300

    Setting Up MongoDB

--- backend/model/user.js
+++ backend/model/user.js
@@ -1,1 +1,28 @@
-j
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    isVerified:{
+        type:Boolean,
+        default:false
+    }
+    
+
+},{
+    timestamps: true
+});
